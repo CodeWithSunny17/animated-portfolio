a92f137a36cb9c27fc54ac8a881f8f0e642c0ef7
Sunny Kumar <sunny274.kr@gmail.com>
2024-02-27 11:26:48 +0530

    commit_day3

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
-import "./navbar.scss"
+import "./navbar.scss";
+import {motion} from "framer-motion";
 
 const Navbar = () => {
+
+  const container = {
+    hidden: { opacity: 0.25, scale: 0.75 },
+    visible: {
+      opacity: 1, 
+      scale: 1, 
+      x:100, 
+      transition:{
+        // type:"spring", 
+        // stiffness:200,      //fluedity
+        // damping:100      //hardness
+        staggerChildren: 0.5
+      }
+    }
+  };
+
   return (
     <div className="navbar">
         {/* Sidebar */}
         <div className="wrapper">
-            <span>Sunny Kumar</span>
+            <motion.span variants={container} initial="hidden" animate="visible">Sunny Kumar</motion.span>
             <div className="social">
                 <a href="#"><img src="/facebook.png" alt="" /></a>
                 <a href="#"><img src="/instagram.png" alt="" /></a>
