67bbd4ee7250e5fd2498f4bd937eaf9a4838622b
Sunny Kumar <sunny274.kr@gmail.com>
2024-03-06 19:11:05 +0530

    commit5

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Cursor from "./component/cursor/Cursor";
 
 const App = () => {
   return <div>
-    <Cursor/>
+    {/* <Cursor/> */}
     <section id="HomePage">
       <Navbar/>
       <Hero/>
@@ -18,8 +18,8 @@ const App = () => {
     <section id="Services"><Parallax type="services"/></section>
     <section><Services/></section>
     <section id="Portfolio"><Parallax type="portfolio"/></section>
-    {/* <Portfolio/> */}
-    {/* <section id="Contact"><Contact/></section> */}
+    <Portfolio/>
+    <section id="Contact"><Contact/></section>
 
   </div>;
 };
--- src/component/sidebar/togglebutton/Togglebutton.jsx
+++ src/component/sidebar/togglebutton/Togglebutton.jsx
@@ -6,7 +6,10 @@ const Togglebutton = (
   )=> {
   
     return (
-      <button onClick={()=>setOpen((prev)=>!prev)}>
+      <button onClick={
+        ()=>setOpen((prev)=>!prev)
+        // console.log("ho rha h!")
+        }>
         <svg width="23" height="23" viewBox="0 0 23 23">
           <motion.path strokeWidth="3" stroke="black" strokeLinecap="round" variants={{closed: {d: "M 2 2.5 L 20 2.5"}, open:{d: "M 3 16.5 L 17 2.5"},}}/>
           <motion.path strokeWidth="3" stroke="black" strokeLinecap="round" d="M 2 9.423 L 20 9.423" variants={{closed: {opacity: 1}, open: {opacity: 0},}}/>
